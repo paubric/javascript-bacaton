5c49d47972524f879ffb90a47261b054a2db5d5a
paubric <paubric@gmail.com>
2019-02-26 20:18:28 +0200

    Working interface

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import Flashcard from "./components/flashcard";
+
+class App extends Component {
+  render() {
+    return <Flashcard />;
+  }
+}
+
+export default App;
